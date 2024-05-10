44f95b73c0ef6d19c61d3dd6aa45cdc823081230
yanirc1981 <cerzurc@hotmail.com>
2024-05-10 10:14:05 -0300

    Se agrega vista de rutas

--- natalie-main/src/App.jsx
+++ natalie-main/src/App.jsx
@@ -9,7 +9,11 @@ import Control from './components/Panel/Control/Control'
 import Financiero from './components/Panel/Financiero/Financiero'
 import ConsentimientoTable from './components/Panel/Pacientes/ConsentimientoTable'
 import PacienteDetail from './components/Panel/Pacientes/PacienteDetail'
-
+import PacienteForm from './components/Panel/Pacientes/PacienteForm'
+import Pacientes from './components/Panel/Pacientes/Pacientes'
+import Perfil from './components/Panel/Perfil/Perfil'
+import Usuarios from './components/Panel/Usuarios/Usuarios'
+import Agenda from './components/Panel/Agenda/Agenda'
 
 function App() {
 
@@ -18,14 +22,18 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
+        <Route path="/perfil" element={<Perfil/>} />
         <Route path="/panel" element={<Panel />} />
+        <Route path="/usuarios" element={<Usuarios />} />
+        <Route path="/agenda" element={<Agenda />} />
         <Route path="/politicas" element={<Politicas />} />
         <Route path="/terminos" element={<Terminos />} />
         <Route path="/control" element={<Control />} />
         <Route path="/financiero" element={<Financiero />} />
         <Route path="/consentimiento" element={<ConsentimientoTable />} />
         <Route path="/detallePaciente" element={<PacienteDetail />} />
-
+        <Route path="/pacienteForm" element={<PacienteForm/>} />
+        <Route path="/pacientes" element={<Pacientes/>} />
 
 
       </Routes>
