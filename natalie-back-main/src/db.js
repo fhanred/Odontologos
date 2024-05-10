5104d1f73c0bf05ca12da098d787836c84b366f9
yanirc1981 <cerzurc@hotmail.com>
2024-05-10 10:45:03 -0300

    se conecta DB

--- natalie-back-main/src/db.js
+++ natalie-back-main/src/db.js
@@ -2,10 +2,12 @@ require('dotenv').config();
 const { Sequelize } = require('sequelize');
 const fs = require('fs');
 const path = require('path');
-const {DB_USER, DB_PASSWORD, DB_NAME, DB_HOST, DB_PORT } = process.env;
+//const {DB_USER, DB_PASSWORD, DB_NAME, DB_HOST, DB_PORT } = process.env;
+
+
 
 const sequelize = new Sequelize(
-  `postgresql://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_NAME}`,
+  "postgres://default:c9vzN8yUJrZb@ep-proud-hat-a4296ipl.us-east-1.aws.neon.tech:5432/verceldb?sslmode=require",
  {
     logging: false, // set to console.log to see the raw SQL queries
     native: false, // lets Sequelize know we can use pg-native for ~30% more speed
